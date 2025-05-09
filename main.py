ad5b8bd154ad91aec77f25177fae527fea2f4b6c
Nathaniel White <nathaniel@debian.mycelia.local>
2025-05-09 11:33:44 -0400

    Added additional functionality to Player. Added Asteroid and AsteroidField classes. Updated main to utilize sprite groups.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import *
 from constants import *
+from player import Player
 
 
 def main():
@@ -9,12 +12,35 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    player = Player(x, y)
+    asteroid_field = AsteroidField()
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
         screen.fill("black")
+        for thing in drawable:
+            thing.draw(screen)
+
+        updatable.update(dt)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
